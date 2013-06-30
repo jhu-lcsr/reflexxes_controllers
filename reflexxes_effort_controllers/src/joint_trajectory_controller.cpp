b91f89f2904fd9cd6f0b17994555ba7e53b12339
Jonathan Bohren <jbo@jhu.edu>
2013-06-30 14:02:28 -0400

    Initial commit, forking from effort_controllers

---
+++ reflexxes_effort_controllers/src/joint_trajectory_controller.cpp
@@ -0,0 +1,206 @@
+/*********************************************************************
+ * Software License Agreement (LGPL License)
+ *
+ *  Copyright (c) 2013, The Johns Hopkins University
+ *  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 3.0 of the License, or (at your option) any later version.
+ *  
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *   
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2008, Willow Garage, Inc.
+ *  Copyright (c) 2012, hiDOF, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ *********************************************************************/
+
+#include "joint_trajectory_controller.h"
+#include <angles/angles.h>
+#include <pluginlib/class_list_macros.h>
+
+namespace reflexxes_effort_controllers {
+
+JointTrajectoryController::JointTrajectoryController()
+: loop_count_(0)
+{}
+
+JointTrajectoryController::~JointTrajectoryController()
+{
+  sub_command_.shutdown();
+}
+
+bool JointTrajectoryController::init(hardware_interface::EffortJointInterface *robot, 
+				   const std::string &joint_name, const control_toolbox::Pid &pid)
+{
+  joint_ = robot->getHandle(joint_name);
+  pid_controller_ = pid;
+
+  // get urdf info about joint
+  urdf::Model urdf;
+  if (!urdf.initParam("robot_description")){
+    ROS_ERROR("Failed to parse urdf file");
+    return false;
+  }
+  joint_urdf_ = urdf.getJoint(joint_name);
+  if (!joint_urdf_){
+    ROS_ERROR("Could not find joint '%s' in urdf", joint_name.c_str());
+    return false;
+  }
+
+  return true;
+}
+
+bool JointTrajectoryController::init(hardware_interface::EffortJointInterface *robot, ros::NodeHandle &n)
+{
+  std::string joint_name;
+  if (!n.getParam("joint", joint_name)) {
+    ROS_ERROR("No joint given (namespace: %s)", n.getNamespace().c_str());
+    return false;
+  }
+
+  control_toolbox::Pid pid;
+  if (!pid.init(ros::NodeHandle(n, "pid")))
+    return false;
+
+  controller_state_publisher_.reset(
+    new realtime_tools::RealtimePublisher<controllers_msgs::JointControllerState>(n, "state", 1));
+
+  sub_command_ = n.subscribe<std_msgs::Float64>("command", 1, &JointTrajectoryController::setCommandCB, this);
+
+  return init(robot, joint_name, pid);
+}
+
+
+void JointTrajectoryController::setGains(const double &p, const double &i, const double &d, const double &i_max, const double &i_min)
+{
+  pid_controller_.setGains(p,i,d,i_max,i_min);
+}
+
+void JointTrajectoryController::getGains(double &p, double &i, double &d, double &i_max, double &i_min)
+{
+  pid_controller_.getGains(p,i,d,i_max,i_min);
+}
+
+std::string JointTrajectoryController::getJointName()
+{
+  return joint_.getName();
+}
+
+// Set the joint position command
+void JointTrajectoryController::setCommand(double cmd)
+{
+  // the writeFromNonRT can be used in RT, if you have the guarantee that 
+  //  * no non-rt thread is calling the same function (we're not subscribing to ros callbacks)
+  //  * there is only one single rt thread
+  command_.writeFromNonRT(cmd);
+}
+
+
+void JointTrajectoryController::starting(const ros::Time& time) 
+{
+  command_.initRT(joint_.getPosition());
+  pid_controller_.reset();
+}
+
+
+void JointTrajectoryController::update(const ros::Time& time, const ros::Duration& period)
+{
+  double command = *(command_.readFromRT());
+
+  double error, vel_error;
+
+  // Compute position error
+  if (joint_urdf_->type == urdf::Joint::REVOLUTE)
+  {
+    angles::shortest_angular_distance_with_limits(joint_.getPosition(),
+						  command, 
+						  joint_urdf_->limits->lower, 
+						  joint_urdf_->limits->upper,
+						  error);
+  }
+  else if (joint_urdf_->type == urdf::Joint::CONTINUOUS)
+  {
+    error = angles::shortest_angular_distance(joint_.getPosition(), command);
+  }
+  else //prismatic
+  {
+    error = command - joint_.getPosition();
+  }
+
+  // Compute velocity error assuming desired velocity is 0
+  vel_error = 0.0 - joint_.getVelocity();
+
+  // Set the PID error and compute the PID command with nonuniform
+  // time step size. This also allows the user to pass in a precomputed derivative error. 
+  double commanded_effort = pid_controller_.computeCommand(error, vel_error, period); 
+  joint_.setCommand(commanded_effort);
+
+
+  // publish state
+  if (loop_count_ % 10 == 0)
+  {
+    if(controller_state_publisher_ && controller_state_publisher_->trylock())
+    {
+      controller_state_publisher_->msg_.header.stamp = time;
+      controller_state_publisher_->msg_.set_point = command;
+      controller_state_publisher_->msg_.process_value = joint_.getPosition();
+      controller_state_publisher_->msg_.process_value_dot = joint_.getVelocity();
+      controller_state_publisher_->msg_.error = error;
+      controller_state_publisher_->msg_.time_step = period.toSec();
+      controller_state_publisher_->msg_.command = commanded_effort;
+
+      double dummy;
+      getGains(controller_state_publisher_->msg_.p,
+               controller_state_publisher_->msg_.i,
+               controller_state_publisher_->msg_.d,
+               controller_state_publisher_->msg_.i_clamp,
+               dummy);
+      controller_state_publisher_->unlockAndPublish();
+    }
+  }
+  loop_count_++;
+}
+
+void JointTrajectoryController::setCommandCB(const std_msgs::Float64ConstPtr& msg)
+{
+  setCommand(msg->data);
+}
+
+} // namespace
+
+PLUGINLIB_EXPORT_CLASS( reflexxes_effort_controllers::JointTrajectoryController, controller_interface::ControllerBase)
---
+++ reflexxes_effort_controllers/src/joint_trajectory_controller.h
@@ -0,0 +1,146 @@
+/*********************************************************************
+ * Software License Agreement (LGPL License)
+ *
+ *  Copyright (c) 2013, The Johns Hopkins University
+ *  All rights reserved.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 3.0 of the License, or (at your option) any later version.
+ *  
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *   
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2008, Willow Garage, Inc.
+ *  Copyright (c) 2012, hiDOF, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ *********************************************************************/
+
+#ifndef EFFORT_CONTROLLERS_JOINT_POSITION_CONTROLLER_H
+#define EFFORT_CONTROLLERS_JOINT_POSITION_CONTROLLER_H
+
+/**
+   @class reflexxes_effort_controllers::JointTrajectoryController
+   @brief Joint Position Controller
+
+   This class controls positon using a pid loop.
+
+   @section ROS ROS interface
+
+   @param type Must be "reflexxes_effort_controllers::JointTrajectoryController"
+   @param joint Name of the joint to control.
+   @param pid Contains the gains for the PID loop around position.  See: control_toolbox::Pid
+
+   Subscribes to:
+
+   - @b command (std_msgs::Float64) : The joint position to achieve.
+
+   Publishes:
+
+   - @b state (controllers_msgs::JointControllerState) :
+     Current state of the controller, including pid error and gains.
+
+*/
+
+#include <ros/node_handle.h>
+#include <urdf/model.h>
+#include <control_toolbox/pid.h>
+#include <control_toolbox/pid_gains_setter.h>
+#include <boost/scoped_ptr.hpp>
+#include <boost/thread/condition.hpp>
+#include <realtime_tools/realtime_publisher.h>
+#include <hardware_interface/joint_command_interface.h>
+#include <controller_interface/controller.h>
+#include <controllers_msgs/JointControllerState.h>
+#include <std_msgs/Float64.h>
+#include <controllers_msgs/JointControllerState.h>
+#include <realtime_tools/realtime_buffer.h>
+
+#include <ReflexxesAPI.h>
+#include <RMLPositionFlags.h>
+#include <RMLPositionInputParameters.h>
+#include <RMLPositionOutputParameters.h>
+
+namespace reflexxes_effort_controllers
+{
+
+class JointTrajectoryController: public controller_interface::Controller<hardware_interface::EffortJointInterface>
+{
+public:
+
+  JointTrajectoryController();
+  ~JointTrajectoryController();
+
+  bool init(hardware_interface::EffortJointInterface*robot, const std::string &joint_name,const control_toolbox::Pid &pid);
+  bool init(hardware_interface::EffortJointInterface *robot, ros::NodeHandle &n);
+
+  /*!
+   * \brief Give set position of the joint for next update: revolute (angle) and prismatic (position)
+   *
+   * \param command
+   */
+  void setCommand(double cmd);
+
+  void starting(const ros::Time& time);
+  
+  /*!
+   * \brief Issues commands to the joint. Should be called at regular intervals
+   */
+  void update(const ros::Time& time, const ros::Duration& period);
+
+  void getGains(double &p, double &i, double &d, double &i_max, double &i_min);
+  void setGains(const double &p, const double &i, const double &d, const double &i_max, const double &i_min);
+
+  std::string getJointName();
+  hardware_interface::JointHandle joint_;
+  boost::shared_ptr<const urdf::Joint> joint_urdf_;
+  realtime_tools::RealtimeBuffer<double> command_;             /**< Last commanded position. */
+
+private:
+  int loop_count_;
+  control_toolbox::Pid pid_controller_;       /**< Internal PID controller. */
+
+  boost::scoped_ptr<
+    realtime_tools::RealtimePublisher<
+      controllers_msgs::JointControllerState> > controller_state_publisher_ ;
+
+  ros::Subscriber sub_command_;
+  void setCommandCB(const std_msgs::Float64ConstPtr& msg);
+};
+
+} // namespace
+
+#endif
